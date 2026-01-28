de29eb6fd93d6e74b8d174ed7421e773949fa3e0
Gustavo Liberatti <liberatti.gustavo@gmail.com>
2026-01-28 08:29:25 -0400

    Init v0.0.2

--- basic4web/middleware/logging.py
+++ basic4web/middleware/logging.py
@@ -1,43 +1,42 @@
-import inspect
 import logging
-import os
 
 import basic4web.config as base_config
 
 
 class CustomLogger(logging.Logger):
     def info(self, msg, *args, **kwargs):
-        frame = inspect.currentframe().f_back
-        caller_method = frame.f_code.co_name
-        filename = os.path.basename(frame.f_globals.get("__file__", ""))
-        lineno = frame.f_lineno
-        super().info(f"[{filename}][{caller_method}][{lineno}] {msg}", *args, **kwargs)
+        kwargs.setdefault("stacklevel", 2)
+        super().info(msg, *args, **kwargs)
 
     def error(self, msg, *args, **kwargs):
-        frame = inspect.currentframe().f_back
-        caller_method = frame.f_code.co_name
-        filename = os.path.basename(frame.f_globals.get("__file__", ""))
-        lineno = frame.f_lineno
-        super().error(f"[{filename}][{caller_method}][{lineno}] {msg}", *args, **kwargs)
+        kwargs.setdefault("stacklevel", 2)
+        super().error(msg, *args, **kwargs)
 
-    def warn(self, msg, *args, **kwargs):
-        frame = inspect.currentframe().f_back
-        caller_method = frame.f_code.co_name
-        filename = os.path.basename(frame.f_globals.get("__file__", ""))
-        lineno = frame.f_lineno
-        super().warning(
-            f"[{filename}][{caller_method}][{lineno}] {msg}", *args, **kwargs
-        )
+    def warning(self, msg, *args, **kwargs):
+        kwargs.setdefault("stacklevel", 2)
+        super().warning(msg, *args, **kwargs)
 
 
-logger = CustomLogger(__name__)
+formatter = logging.Formatter(
+    "%(asctime)s - %(process)d/%(thread)d - v0.0.2 - %(levelname)s - "
+    "[%(filename)s][%(funcName)s][%(lineno)d] %(message)s"
+)
+logging.setLoggerClass(CustomLogger)
+
+logger = logging.getLogger(__name__)
 
 level = getattr(logging, base_config.get("LOG_LEVEL"), logging.INFO)
 logger.setLevel(level)
+
 console_handler = logging.StreamHandler()
 console_handler.setLevel(level)
-formatter = logging.Formatter(
-    f"%(asctime)s - %(process)d/%(thread)d - {base_config.get('CORE_VERSION')} - %(levelname)s - %(message)s"
-)
 console_handler.setFormatter(formatter)
+
 logger.addHandler(console_handler)
+
+# evita duplicação
+logger.propagate = False
+
+# silencia libs externas
+logging.getLogger("urllib3").setLevel(logging.WARNING)
+logging.getLogger("requests").setLevel(logging.WARNING)
