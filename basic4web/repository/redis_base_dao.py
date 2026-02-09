b9d6abeb25deee7323f756fe35544fd8f797c940
Gustavo Liberatti <liberatti.gustavo@gmail.com>
2026-02-09 15:28:12 -0400

    Changes to be committed: 	modified:   basic4web/repository/redis_base_dao.py

--- basic4web/repository/redis_base_dao.py
+++ basic4web/repository/redis_base_dao.py
@@ -1,15 +1,14 @@
 import json
-
 import redis
 
 
 class RedisDAO:
-    def __init__(self, host="127.0.0.1", port=6379, password=None, conn=None, db=0):
+    def __init__(self, host="127.0.0.1", port=6379, password=None, db=0):
         self.host = host
         self.port = port
         self.password = password
         self.db = db
-        self.conn = conn
+        self.conn = None
 
     def connect(self):
         if not self.conn:
@@ -18,9 +17,13 @@ class RedisDAO:
                 port=self.port,
                 password=self.password,
                 db=self.db,
-                decode_responses=True
+                decode_responses=True,
             )
             self.conn.ping()
+
+    def _ensure_connection(self):
+        if not self.is_connected():
+            self.connect()
 
     def is_connected(self):
         try:
@@ -28,28 +31,40 @@ class RedisDAO:
         except (redis.ConnectionError, AttributeError):
             return False
 
-    def persist(self, k, v, expire=None):
-        if expire:
-            self.conn.set(k, v, ex=expire)
-        else:
-            self.conn.set(k, v)
+    def persist(self, key, value, expire=None):
+        self._ensure_connection()
+        payload = json.dumps(value)
+        self.conn.set(key, payload, ex=expire)
 
-    def get_by_id(self, k):
-        return self.conn.get(k)
+    def get_by_id(self, key):
+        self._ensure_connection()
+        value = self.conn.get(key)
+        return json.loads(value) if value else None
 
-    def delete(self, k):
-        self.conn.delete(k)
+    def delete(self, key):
+        self._ensure_connection()
+        return self.conn.delete(key)
 
     def get_keys_by_prefix(self, pattern="*"):
+        self._ensure_connection()
         return list(self.conn.scan_iter(match=pattern))
 
     def get_items_by_prefix(self, pattern="*"):
-        items = list()
+        items = []
+
         for key in self.conn.scan_iter(match=pattern):
-            j = self.conn.get(key)
-            i = json.loads(j)
-            i.update({"_id": key})
-            items.append(i)
+            raw = self.conn.get(key)
+            if not raw:
+                continue
+
+            try:
+                data = json.loads(raw)
+            except json.JSONDecodeError:
+                continue
+
+            data["_id"] = key
+            items.append(data)
+
         return items
 
     def __enter__(self):
